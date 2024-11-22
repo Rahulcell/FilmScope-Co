1664050fb4322a698d1dbd4618c8802c5cd7eb5a
Rahulcell <rahuljakkula6@gmail.com>
2024-11-23 05:27:01 +0530

    added login and logout functionality

--- password request.js
+++ password request.js
@@ -1,14 +1,9 @@
 import { getAuth, sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 
-// Firebase Configuration
-require('dotenv').config(); // Load environment variables
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY, // Use environment variable
-  authDomain: "filmscope-915fb.firebaseapp.com",
-  projectId: "filmscope-915fb",
-  storageBucket: "filmscope-915fb.firebasestorage.app",
-};
+import firebaseConfig from './config.js';
+
+console.log("Firebase Config:", firebaseConfig);
 
 
 // Initialize Firebase
